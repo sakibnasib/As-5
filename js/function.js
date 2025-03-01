00ea293e8773fe80143392a39047cd528a0f8545
sakib11 <abcd.sakib50@gmail.com>
2025-03-01 21:33:09 +0600

    first commit

---
+++ js/function.js
@@ -0,0 +1,35 @@
+document.getElementById("btn-1").addEventListener("click", function() {
+    let button = document.getElementById("btn-1");
+    let title = document.getElementById("title").textContent;  
+    completeTask(title, button);
+});
+
+document.getElementById("btn-2").addEventListener("click", function() {
+    let button = document.getElementById("btn-2");
+    let title = document.getElementById("title-2").textContent;  
+    completeTask(title, button);
+});
+
+document.getElementById("btn-3").addEventListener("click", function() {
+    let button = document.getElementById("btn-3");
+    let title = document.getElementById("title-3").textContent;  
+    completeTask(title, button);
+});
+
+document.getElementById("btn-4").addEventListener("click", function() {
+    let button = document.getElementById("btn-4");
+    let title = document.getElementById("title-4").textContent;  
+    completeTask(title, button);
+});
+
+document.getElementById("btn-5").addEventListener("click", function() {
+    let button = document.getElementById("btn-5");
+    let title = document.getElementById("title").textContent;  
+    completeTask(title, button);
+});
+
+document.getElementById("btn-6").addEventListener("click", function() {
+    let button = document.getElementById("btn-6");
+    let title = document.getElementById("title-6").textContent;  
+    completeTask(title, button);
+});
---
+++ js/index.js
@@ -0,0 +1,60 @@
+document.getElementById("new-tab").addEventListener("click",function(){
+    window.location.href="./Dic.html"
+});
+
+
+function completeTask(taskname, button) {
+    alert(taskname);
+
+    let now = new Date();
+    let timestring = now.toLocaleTimeString();
+
+    let taskCount = document.getElementById("count-task");
+    let numberCount = document.getElementById("number-count");
+    let container = document.getElementById("container");
+    
+
+    let currentCount = parseInt(taskCount.textContent) || 0;
+    taskCount.textContent = currentCount - 1;
+    let currentNumber = parseInt(numberCount.textContent) || 0;
+    numberCount.textContent = currentNumber + 1;
+
+    let taskInfo = document.createElement("p");
+    taskInfo.classList.add("bg-[#F4F7FF]")
+    taskInfo.classList.add("mt-2");
+    taskInfo.classList.add("font-semibold")
+    taskInfo.textContent = `You have completed the Task ${taskname} at ${timestring}`;
+    container.appendChild(taskInfo);
+
+    button.disabled = true;
+    button.style.backgroundColor = "gray";
+}
+
+ document.getElementById("clearBtn").addEventListener("click",function(){
+    const hide=document.getElementById("container");
+    hide.classList.add("hidden")
+ })
+
+
+// date
+let today=new Date();
+let formatteDate=today.toLocaleDateString();
+document.getElementById("date").innerText=formatteDate;
+
+const change = document.getElementById("color");
+let randomNum = function () {
+    return Math.floor(Math.random() * 256);
+};
+let changeColor = function () {
+    let randomColor = `rgb(${randomNum()}, ${randomNum()}, ${randomNum()})`;
+    document.body.style.backgroundColor = randomColor;
+};
+if (change) {
+    change.addEventListener("click", changeColor);
+}
+
+
+
+
+
+
